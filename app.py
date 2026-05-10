b8ce60fd6f9e181167b5e25fb640e74d898f8fe2
gabriel <g4bopy@gmail.com>
2026-05-09 22:48:49 -0300

    Grandes cambios generales al sistema de archivos

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, jsonify, request, render_template, redirect, send_file
+# IMPORTS 
+from flask import Flask, jsonify, request, render_template, redirect, send_file, session
 from flask_socketio import SocketIO, emit, disconnect
 import gevent
 import os
@@ -6,23 +7,27 @@ from dotenv import load_dotenv
 load_dotenv()
 import secrets
 
+# -------------------------------------------------|
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv('SECRET_KEY')
 socketio = SocketIO(app, async_mode='gevent')
 port = int( os.getenv('PORT', '9000') )
-
 #SERVIDOR INICIADO 
 print(f"SERVIDOR INICIADO EN PUERTO: {port}")
-
+# -------------------------------------------------|
 
 txt = ''
 ult_arch = ''
 
+# -------------------------------------------------|
+#                     Funciones                    |
+# -------------------------------------------------|
 def cargar_nombre():
+    """
+    Cargar el nombre del archivo alojado
+    """
     global ult_arch
     try:
-
-
         with open('uploads/info.txt', 'r') as f:
             ult_arch = f.read()
     except FileNotFoundError:
@@ -30,24 +35,9 @@ def cargar_nombre():
         with open('uploads/info.txt', "w") as f:
             f.write("")
 
-
-
-
+# Carga el nombre del ultimo archivo al iniciar servidor
 cargar_nombre()
 
-def verificar_video():
-    cargar_nombre()
-    if '.mp4' in ult_arch:
-        return True
-    else:
-        return False
-
-def verificar_imagen():
-    cargar_nombre()
-    if '.png' in ult_arch or '.jpg' in ult_arch:
-        return True
-    else:
-        return False
 
 @app.route('/')
 def index():
@@ -58,7 +48,7 @@ def upload():
     archivo = request.files['archivo']
     archivo.save('uploads/ultimo')
     with open('uploads/info.txt','w') as f:
-        f.write(archivo.filename )
+        f.write(archivo.filename or 'noname')
     cargar_nombre()
     socketio.emit('ult_archivo',ult_arch)
     return jsonify({'ok':True})
@@ -69,26 +59,22 @@ def download():
 
 @app.route('/video',methods=['GET'])
 def video():
-    if '.mp4' in ult_arch:
-        print("es un video")
-        return send_file('uploads/ultimo', download_name=ult_arch, as_attachment=True)
-    else:
-        print("No es un video")
-        return '404'
+    return send_file('uploads/ultimo', download_name=ult_arch, as_attachment=True)
 
 @app.route('/imagen', methods=['GET'])
 def imagen():
-    if '.jpg' in ult_arch or '.png' in ult_arch:
-        return send_file('uploads/ultimo', download_name=ult_arch, as_attachment=True)
-    else:
-        return '404'
+    return send_file('uploads/ultimo', download_name=ult_arch, as_attachment=True)
+
+@app.route('/audio', methods=['GET'])
+def audio():
+    return send_file('uploads/ultimo', download_name=ult_arch, as_attachment=True)
 
 # -------------------------------------------------------------------|
 def tipo_de_archivo(archivo):
     formats = {
             'video': ['mp4','avi'],
             'imagen': ['png', 'jpg', 'jepg'],
-            'sonido': ['mp3']
+            'audio': ['mp3']
             }
     for types in formats: # Itera sobre todos los tipos de archivos
         for frmts in formats[types]: # itera por cada formato
@@ -100,19 +86,18 @@ def tipo_de_archivo(archivo):
 #--------------------------------------------------------------------|
 def enviar_archivo(archivo):
     tipo = tipo_de_archivo(archivo)
-    socketio.emit('cargar_archivo',{'tipo': tipo, 'ruta': f'/{tipo}'})
+    socketio.emit('cargar_archivo',{'tipo': tipo, 'ruta': f'/{tipo}'}, to=session.get('usr_sid'))
 
 
 # -------------------------------------------------------------------|
 
-@socketio.on('verific_video')
-def verific_video():
-    emit("video", verificar_video(),broadcast=True)
+@socketio.on('verificar_archivo_disponible')
+def handle_verificar_archivo_disponible():
+    enviar_archivo(ult_arch)
 
-@socketio.on('verific_imagen')
-def verific_imagen():
-    emit('imagen',verificar_imagen(),broadcast=True)
 
+
+# -------------------------------------------------------------------------|
 @socketio.on('txt_change')
 def handle_txtChange(data):
     global txt
@@ -120,11 +105,15 @@ def handle_txtChange(data):
     emit("txt_recive",data, broadcast=True)
     enviar_archivo(ult_arch)
 
+# *************************************************************************|
 @socketio.on('connect')
 def handle_connect():
+    if not 'usr_sid' in session:
+        session['usr_sid'] = request.sid # type: ignore
     cargar_nombre()
     emit('ult_archivo', ult_arch)
     emit('txt_recive', txt)
 
+# *************************************************************************|
 if __name__ == '__main__':
-    socketio.run(app,host='0.0.0.0', debug=False, port=9000)
+    socketio.run(app,host='0.0.0.0', debug=False, port=port)
--- static/js/script.js
+++ static/js/script.js
@@ -1,16 +1,62 @@
+// Coneccion con SocketIo
 socket = io();
+// -------------------------------------------------------------|
 const codigo = document.querySelector("#codigo");
 const txt_area = document.querySelector("#txt_area_paste");
 const progress_barr = document.querySelector("#progress_barr");
 const porcentaje_barr = document.querySelector("#porcentaje");
-const video_a = document.querySelector("#video");
 const imagen_a= document.querySelector("#imagen");
 const inp_file = document.querySelector('#input_file');
 const inp_file_btn = document.querySelector('.btn-subir');
+const contenedor_multimedia = document.querySelector('.contenedor_multimedia');
+// ------------------------------------------------------------|
+
+// ------------------------------------------------------------|
+function crear_elemento_media(tipo){
+  let elemento_multimedia ;
+  contenedor_multimedia.replaceChildren();
+  if (tipo == 'video' || tipo == 'audio'){
+    elemento_multimedia = document.createElement(`${tipo}`);
+    elemento_multimedia.src = `/${tipo}`;
+    elemento_multimedia.className = `${tipo}`;
+    elemento_multimedia.controls = true;
+    contenedor_multimedia.appendChild(elemento_multimedia);
+  }
+  else{
+    elemento_multimedia = document.createElement('img');
+    elemento_multimedia.src = '/imagen';
+    elemento_multimedia.className = 'imagen';
+    contenedor_multimedia.appendChild(elemento_multimedia);
+  }
+  ;
+};
+// --------------------------------------------------------------------|
+
+socket.on('cargar_archivo', (data)=>{
+  console.log(data);
+  crear_elemento_media(data.tipo);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// --------------------------------------------------------------------|
 
 inp_file.addEventListener('change', ()=>{
   inp_file_btn.textContent = `Archivo elegido: ${inp_file.files[0]?.name}`
 });
+
+// -----------------------------------------------------------------|
 
 const copiar_btn = document.querySelector("#btn_copiar");
 copiar_btn.addEventListener('click', ()=>{
@@ -23,7 +69,7 @@ copiar_btn.addEventListener('click', ()=>{
     document.body.removeChild(temp);
 });
 
-
+// -----------------------------------------------------------------|
 
 txt_area.addEventListener("input",()=>{
     socket.emit("txt_change",txt_area.value)
@@ -37,35 +83,14 @@ socket.on('txt_recive', (data)=>{
     codigo.innerHTML = result.value;
 });
 
-socket.emit("verific_video");
-socket.on("video", (data)=>{
-    if (data){
-        video_a.style.display = "inline-block";
-        video_a.load();
-    }
-    else{
-        video_a.style.display = "none"
-    }
-});
-
-socket.emit('verific_imagen');
-socket.on('imagen',(data)=>{
-  if (data){
-    imagen_a.style.display = 'inline-block';
-    imagen_a.src = '/imagen?' + Date.now();
-  }
-  else{
-    imagen_a.style.display = 'none';
-  }
-});
 
 
 socket.on('ult_archivo', (data)=>{
     document.getElementById('txt_archivo').innerHTML = `<strong>Archivo disponible:</strong> ${data}`;
-    socket.emit("verific_video");
+    socket.emit("verificar_archivo_disponible");
 });
 
-//SUBIR ARCHIVO
+//SUBIR ARCHIVO --------------------------------------------------|
 const xhr = new XMLHttpRequest();
 xhr.upload.onprogress = function(e){
     const porcentaje = Math.round((e.loaded/e.total)* 100);
@@ -77,6 +102,7 @@ xhr.onload = function(){
     const respuesta = JSON.parse(xhr.responseText);
     console.log(respuesta);
 };
+// ---------------------------------------------------------------|
 
 const input_file = document.querySelector("#input_file");
 
@@ -86,8 +112,5 @@ function subir(){
     xhr.open('POST', '/upload');
     xhr.send(formData);
 };
-
-socket.on('cargar_archivo',(data)=>{
-  console.log(data.tipo,data.ruta)
-});
+// ---------------------------------------------------------------|
 
